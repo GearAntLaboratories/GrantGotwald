b41e0b4b69a78768e7705048bb058cb0128415a8
grantg <grantgotwald@gmail.com>
2016-02-17 13:30:56 -0600

    Initial commit

---
+++ js/scroll.js
@@ -0,0 +1,19 @@
+/**
+ * Created by grantgotwald on 2/16/16.
+ */
+
+$(function() {
+    $('a[href*="#"]:not([href="#myCarousel"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
+
