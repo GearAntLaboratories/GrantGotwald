e3da0af34f611bf90ac13503d8d8382c65422191
Grant <grantgotwald@gmail.com>
2016-03-15 22:33:15 -0500

    Updated index.html

--- js/scroll.js
+++ js/scroll.js
@@ -3,7 +3,7 @@
  */
 
 $(function() {
-    $('a[href*="#"]:not([href="#myCarousel"])').click(function() {
+    $('a[href*="#section-"]').click(function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
@@ -16,4 +16,5 @@ $(function() {
         }
     });
 });
+
 
